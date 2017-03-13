262c3f72f1d9a13e21bc6cf709958076a5ce7395
kkooprojects <keegankoo118@gmail.com>
2017-03-13 06:31:40 -0400

    Initial Setup

---
+++ levels.js
@@ -0,0 +1,149 @@
+levels = [];
+
+levels[0] = {
+	rows: 5,
+	cols: 16,
+	tiles: [
+		'WWWWHHHFFFFFWWWW',
+		'FFFWHHHHFFFFWFFF',
+		'FFFFFHHHFFFFFFFF',
+		'FFFWHHHHFFFFWFFF',
+		'WWWWHHHFFFFFWWWW'
+	],
+	blocks: [
+		{
+			row: 1,
+			col: 9,
+			width: 2,
+			height: 3
+		}
+	],
+	start: {
+		row: 2,
+		col: 14
+	},
+	end: {
+		row: 2,
+		col: 1
+	}
+};
+
+levels[1] = {
+	rows: 13,
+	cols: 18,
+	tiles: [
+		'FFFFFFFFFFFFFFFFFF',
+		'FFFWWWWWFWWWWFWFFF',
+		'FFFWFFHHHWHFFFWFFF',
+		'FFFWFFHHWWHFFFWFFF',
+		'FFFWFFHHHHHFFFWFFF',
+		'FFFWFFHHHHHFFFWFFF',
+		'FFFWFFHHHHHFFFWFFF',
+		'FFFWFFHWHHHWFWWFFF',
+		'FFFWFFFWFFFWFFWFFF',
+		'FFFWFFFWFFFWFFWFFF',
+		'FFFWFFFWFFFFFFWFFF',
+		'FFFWFFFWFFFFFFWFFF',
+		'FFFWFFFWFFFFFFWFFF'
+	],
+	blocks: [
+		{
+			row: 0,
+			col: 9,
+			width: 4,
+			height: 1
+		},
+		{
+			row: 2,
+			col: 12,
+			width: 1,
+			height: 1
+		},
+		{
+			row: 3,
+			col: 12,
+			width: 1,
+			height: 3
+		},
+		{
+			row: 3,
+			col: 13,
+			width: 1,
+			height: 1
+		},
+		{
+			row: 6,
+			col: 12,
+			width: 1,
+			height: 1
+		},
+		{
+			row: 7,
+			col: 4,
+			width: 2,
+			height: 1
+		},
+		{
+			row: 7,
+			col: 12,
+			width: 1,
+			height: 1
+		},
+		{
+			row: 8,
+			col: 9,
+			width: 1,
+			height: 2
+		},
+		{
+			row: 8,
+			col: 10,
+			width: 1,
+			height: 1
+		},
+		{
+			row: 10,
+			col: 6,
+			width: 1,
+			height: 1
+		},
+		{
+			row: 10,
+			col: 10,
+			width: 1,
+			height: 1
+		},
+		{
+			row: 10,
+			col: 12,
+			width: 1,
+			height: 1
+		},
+		{
+			row: 11,
+			col: 9,
+			width: 1,
+			height: 1
+		},
+		{
+			row: 11,
+			col: 11,
+			width: 1,
+			height: 1
+		},
+		{
+			row: 11,
+			col: 12,
+			width: 1,
+			height: 2
+		}
+	],
+	start: {
+		row: 11,
+		col: 16
+	},
+	end: {
+		row: 11,
+		col: 1
+	}
+};
